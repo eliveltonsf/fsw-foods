f15e1aa4986e0290284978022c6c0ade6188867e
eliveltonsf <elivelton.seven@gmail.com>
2024-10-16 15:05:18 -0300

    fix: Correction in shadcn utility path

--- app/_components/ui/card.tsx
+++ app/_components/ui/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "@/app/lib/utils";
+import { cn } from "../../_lib/utils";
 
 const Card = React.forwardRef<
   HTMLDivElement,
@@ -78,9 +78,9 @@ CardFooter.displayName = "CardFooter";
 
 export {
   Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
   CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 };
